7b77b32544b06c305fc261299588ec52c6ad2009
hoquocbao23 <hoquocbao6123@gmail.com>
2025-05-27 15:52:07 +0700

    dashboard

---
+++ metro-system-fe/src/pages/dashboard/promotion/promotion-dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PromotionDashboard = () => {
+    return (    
+        <div>
+            <h1>Promotion Dashboard</h1>
+        </div>
+    )
+}
+
+export default PromotionDashboard;
